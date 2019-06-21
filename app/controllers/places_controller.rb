e03181ae2ac738c3f4afff0747c1e4b09e1b3d96
Reba Pipes <rebablackbox@gmail.com>
2019-06-21 18:30:41 +0000

    fixed syntax error

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
   def index
-    @places = Place.order(:id).paginate(:page => params[:page], :per_page => 2
+    @places = Place.order(:id).paginate(:page => params[:page], :per_page => 2)
   end
 end

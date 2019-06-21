d6868c0d2ddd0af5d3bfa3c4dc72b0c5d72cc1bf
Reba Pipes <rebablackbox@gmail.com>
2019-06-21 18:24:16 +0000

    updated places controller

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
   def index
-    @places = Place.paginate(page: params[:page], per_page: 5)
+    @places = Place.order(:id).paginate(:page => params[:page], :per_page => 2
   end
 end

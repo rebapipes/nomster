f210abbf575e7efff39a4e32b27b193b5af4063d
Reba Pipes <rebablackbox@gmail.com>
2019-06-25 20:15:29 +0000

    added edit form to place page

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -13,6 +13,9 @@ class PlacesController < ApplicationController
   def show
     @place = Place.find(params[:id])
   end
+  def edit
+    @place = Place.find(params[:id])
+  end
   private
   def place_params
     params.require(:place).permit(:name, :description, :address)

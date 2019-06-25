ad6cd7f7fafedef3b7e7da63bf90b81860b91432
Reba Pipes <rebablackbox@gmail.com>
2019-06-25 20:21:01 +0000

    updated the place data

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -16,6 +16,11 @@ class PlacesController < ApplicationController
   def edit
     @place = Place.find(params[:id])
   end
+  def update
+    @place = Place.find(params[:id])
+    @place.update_attributes(place_params)
+    redirect_to root_path
+  end
   private
   def place_params
     params.require(:place).permit(:name, :description, :address)

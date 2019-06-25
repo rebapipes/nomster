094b5481d6193b05164e0733c021cc4435502333
Reba Pipes <rebablackbox@gmail.com>
2019-06-25 20:05:54 +0000

    added detail to place page

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -10,8 +10,10 @@ class PlacesController < ApplicationController
     current_user.places.create(place_params) 
     redirect_to root_path
   end
+  def show
+    @place = Place.find(params[:id])
+  end
   private
-
   def place_params
     params.require(:place).permit(:name, :description, :address)
   end

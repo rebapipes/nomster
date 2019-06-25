c7e89ccf58f4e1beb4a433ee34da1f8c62e370a7
Reba Pipes <rebablackbox@gmail.com>
2019-06-25 20:26:38 +0000

    added feature to destroy a place

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -21,8 +21,13 @@ class PlacesController < ApplicationController
     @place.update_attributes(place_params)
     redirect_to root_path
   end
+  def destroy
+    @place = Place.find(params[:id])
+  end
   private
   def place_params
     params.require(:place).permit(:name, :description, :address)
+    @place.destroy
+    redirect_to root_path
   end
 end

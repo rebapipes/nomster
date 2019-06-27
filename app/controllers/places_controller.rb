e9fd7ee7aa67350c40e329499d161d2a2d7705e5
Reba Pipes <rebablackbox@gmail.com>
2019-06-27 02:13:24 +0000

    updated user authentication

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,30 +1,39 @@
 class PlacesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
+
   def index
     @places = Place.order(:id).paginate(:page => params[:page], :per_page => 2)
   end
+  
   def new
     @place = Place.new
   end
-   def create
-    current_user.places.create(place_params) 
-    redirect_to root_path
+   
+  def create
+  current_user.places.create(place_params)
+  redirect_to root_path
   end
+  
   def show
     @place = Place.find(params[:id])
   end
+  
   def edit
     @place = Place.find(params[:id])
   end
+  
   def update
     @place = Place.find(params[:id])
     @place.update_attributes(place_params)
     redirect_to root_path
   end
+  
   def destroy
     @place = Place.find(params[:id])
   end
+  
   private
+  
   def place_params
     params.require(:place).permit(:name, :description, :address)
     @place.destroy
--- db/migrate/20190625190337_alter_places_add_user_id_column.rb
+++ db/migrate/20190625190337_alter_places_add_user_id_column.rb
@@ -1,4 +1,4 @@
-class AlterPlacesAddUserIdColumn < ActiveRecord::Migration[5.2]
+class AlterPlacesAddUserIdColumn < ActiveRecord::Migration[5.0]
   def change
     add_column :places, :user_id, :integer
     add_index :places, :user_id

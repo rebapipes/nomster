59b0f306aa95875e52f1ca6d94d7c037cadea76b
Reba Pipes <rebablackbox@gmail.com>
2019-06-29 15:17:13 +0000

    added more validations for place rb

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,6 @@
-class Place < ApplicationRecord
+vagrant ssclass Place < ApplicationRecord
   belongs_to :user
-  validates :name, presence: true
+  validates :name, presence: true, length: { minimum: 3, too_short: "%{count} characters is the maximum allowed" }
+  validates :address, presence: true
+  validates :description, presence: true
 end

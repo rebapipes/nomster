4a4c75f110354304e001671d6f3515f41cbeca2d
Reba Pipes <rebablackbox@gmail.com>
2019-07-02 15:40:47 +0000

    added comments in database

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,7 +1,10 @@
 class Place < ApplicationRecord
   belongs_to :user
+  has_many :comments
+
   geocoded_by :address
   after_validation :geocode
+
   validates :name, presence: true, length: { minimum: 3, 
     too_short: "%{count} characters is the minimum allowed" }
   validates :address, :description, presence: true

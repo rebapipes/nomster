d601c7a4be40122fff934856fdf9da043b23836f
Reba Pipes <rebablackbox@gmail.com>
2019-07-05 21:56:17 +0000

    adjusted the mailer

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,8 +1,11 @@
 class NotificationMailer < ApplicationMailer
   default from: "no-reply@nomsterapp.com"
 
-  def comment_added
-    mail(to: "rebablackbox@gmail.com",
+  def comment_added(comment)
+    @place = comment.place
+    @place_owner = @place.user
+
+    mail(to: @place_owner.email,
          subject: "A comment has been added to your place")
   end
 end

09278e71cd18e66a332d315673bf088c38a01a3d
Reba Pipes <rebablackbox@gmail.com>
2019-07-05 23:16:12 +0000

    test

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-  config.action_mailer.delivery_method = :test
+  config.action_mailer.delivery_method = :smtp
 
   #config.action_mailer.smtp_settings = {
     #address: "smtp.gmail.com",

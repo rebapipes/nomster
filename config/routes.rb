46b9c76d56d2651809e1b3751a9905f1395d5daa
Reba Pipes <rebablackbox@gmail.com>
2019-07-06 21:54:47 +0000

    created user dashboard page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root 'places#index'
   resources :places do
-    resources :comments, :photos, only: :create
+    resources :comments, only: :create
+    resources :photos, only: :create
   end
+  resources :users, only: :show
 end

7541a4c6841ed6f96eff6fa3ece2d2c3c1525ef6
Reba Pipes <rebablackbox@gmail.com>
2019-07-06 20:11:06 +0000

    updated development rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,8 @@
 Rails.application.configure do
   config.action_mailer.delivery_method = :test
 
+  #config.action_mailer.delivery_method = :smtp
+
   #config.action_mailer.smtp_settings = {
     #address: "smtp.gmail.com",
     #port: 587,

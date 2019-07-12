60f9eb95d9c7354e48041594e22e8a577a502efc
Reba Pipes <rebablackbox@gmail.com>
2019-07-12 15:48:27 +0000

    error fixing

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,7 +3,7 @@ class PhotosController < ApplicationController
 
   def create
     @place = Place.find(params[:place_id])
-    @place.photos.create(photos_params.merge(user: current_user))
+    @place.photos.create(photo_params.merge(user: current_user))
     redirect_to place_path(@place)
   end
 
